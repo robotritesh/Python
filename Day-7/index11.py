be8ef6ca4853f4592e53cc36f0084cf219854831
Ritesh Dhote <dhoteritesh66@gmail.com>
2024-10-25 22:56:51 +0530

    add file

---
+++ Day-7/index11.py
@@ -0,0 +1,7 @@
+# Simple list comprehension
+squares = [x ** 2 for x in range(10)]
+print("Squares:", squares)
+
+# List comprehension with condition
+evens = [x for x in range(20) if x % 2 == 0]
+print("Evens:", evens)
