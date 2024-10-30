fb8b5eea44a85f80dec688f5a168db8d551e6b2c
Ritesh Dhote <dhoteritesh66@gmail.com>
2024-10-30 23:53:16 +0530

    add file

--- Day-4/index11.py
+++ Day-4/index11.py
@@ -1,9 +1,13 @@
-# Generate Fibonacci sequence
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+# # Generate Fibonacci sequence
+# def fibonacci(n):
+#     a, b = 0, 1
+#     for _ in range(n):
+#         yield a
+#         a, b = b, a + b
 
-fib_seq = list(fibonacci(10))
-print("Fibonacci Sequence:", fib_seq)
+# fib_seq = list(fibonacci(10))
+# print("Fibonacci Sequence:", fib_seq)
+
+
+
+# addet data
