c61cf7c794329c4cd7baa94b4e21c1e1daba5a8b
Ritesh Dhote <dhoteritesh66@gmail.com>
2024-11-10 23:30:14 +0530

    add file

---
+++ Day-10/index9.py
@@ -0,0 +1,6 @@
+names = ["Alice", "Bob", "Charlie"]
+scores = [85, 90, 95]
+
+# Combine names and scores into a dictionary
+score_dict = dict(zip(names, scores))
+print("Score Dictionary:", score_dict)
