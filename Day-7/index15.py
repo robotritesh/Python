88be4f41dfd13874d3938b1bca71475e73c24bba
Ritesh Dhote <dhoteritesh66@gmail.com>
2025-01-18 23:39:08 +0530

    add file

---
+++ Day-7/index15.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+student1 = Student("Alice", 20)
+print("Name:", student1.name)
+print("Age:", student1.age)
+cd D
